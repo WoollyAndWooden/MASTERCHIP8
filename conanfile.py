b8945385bbe8bb351e37e58cfc76229f2d8a02ea
daniel <daniel2000r@gmail.com>
2025-11-14 01:28:49 +0100

    Fix up conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class MASTERCHIP8(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "qtwayland": [True, False], "qtwebengine": [True, False]}
     default_options = {"shared": True, "qtwayland": False, "qtwebengine": True}  # default False
-    generators = "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
     exports_sources = "*"
 
     def config_options(self):
@@ -17,11 +17,6 @@ class MASTERCHIP8(ConanFile):
             self.options.qtwebengine = False
         else:
             self.options.qtwayland = False  # ensure disabled for Windows/macOS
-
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.variables["QT_FEATURE_int128"] = "ON"  # set INT128 support
-        tc.generate()
 
     def requirements(self):
         self.requires("qt/6.8.3")
